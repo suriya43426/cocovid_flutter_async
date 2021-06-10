1edf65957e8091ad52fd2288baa65316b4999eec
suriya chayatummagoon <suriya43426@gmail.com>
2021-06-10 16:51:19 +0700

    create => src => config => create ThemeClass
    using Stack and Drawing Layout suck as header, form, forgot Password, SSO, register
    
    in Stack (fit, children: [Container, Columm Layout])

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:covid19/src/config/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'my_home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,12 +9,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Covid Today'),
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Container(
+              decoration: BoxDecoration(gradient: Theme2.gradient),
+            ),
+            Column(
+              children: [
+                Text('header'),
+                Text('form'),
+                Text('forgot'),
+                Text('SSO'),
+                Text('register')
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/src/config/theme.dart
+++ lib/src/config/theme.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 
-class Theme {
-  const Theme();
-
-  static const Color gradientStart = const Color(0xff57CBE6);
-  static const Color gradientEnd = const Color(0xff5649E6);
+class Theme2 {
+  const Theme2();
 
   static const gradient = const LinearGradient(
-      colors: [gradientStart, gradientEnd],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-      stops: [0.0, 1.0]);
+    colors: [Color(0xff57CBE6), Color(0xff5649E6)],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    stops: [0.0, 1.0],
+  );
 }
