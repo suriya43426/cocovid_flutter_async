01615e5e32ce14854c25482db2b5ded9a67ccdc9
suriya chayatummagoon <suriya43426@gmail.com>
2021-06-13 00:05:30 +0700

    update font awesom

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:covid19/src/config/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,6 +21,9 @@ class MyApp extends StatelessWidget {
             Column(
               children: [
                 Text('header'),
+                IconButton(onPressed: (){
+                  print('okok');
+                }, icon: FaIcon(FontAwesomeIcons.gamepad)),
                 Text('form'),
                 Text('forgot'),
                 Text('SSO'),
